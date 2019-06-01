d55ff22c6d3d137c20a8fd2fba50aa60ec4bc13e
Ivan Mendoza <theeachtra2002@yahoo.com>
2019-05-31 22:04:39 -0500

    change server settings

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+const path = require("path");
 const express = require("express");
 const routes = require("./routes");
 // const path = require('path');
 
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("Client/build"));
+  app.use(express.static(path.join(__dirname, "Client/build")));
 }
 
 app.use(routes);
@@ -26,5 +27,5 @@ connection.once("open", function() {
 });
 
 app.listen(PORT, function() {
-  console.log("app listening on port 3001");
+  console.log("app listening on port 8000");
 });
