196e5ce87a95f8b2b1f49b85a90bb25b2e427472
Ivan Mendoza <theeachtra2002@yahoo.com>
2019-06-06 09:50:26 -0500

    change controllers routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,15 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 const messageRoutes = require("./messages");
 
-
 router.use("/messages", messageRoutes);
 
-
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "Client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../..Client/build", "index.html"));
 });
-
-
-
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-
-
 router.use("/api", apiRoutes);
 
 //this sends default page if above route doesn't hit
 router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "Client/build","index.html"))
+  res.sendFile(path.join(__dirname, "../Client/build", "index.html"))
 );
-
-
-
 
 module.exports = router;
